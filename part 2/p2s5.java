6099eb22c249fa1c5ba5b59c3f16912d6ddb8fc3
Saumyahc15 <23dcs014@charusat.edu.in>
2024-07-30 10:41:44 +0530

    Create p2s5.java

---
+++ part 2/p2s5.java
@@ -0,0 +1,15 @@
+class p2s5
+{
+public static void main(String []args)
+{
+String s="CHARUSAT UNIVERSITY";
+int n=s.length();
+System.out.println("Length of the string is:"+n);
+
+String x=s.replace('H','S');
+System.out.println("After replacing string is:"+x);
+
+String y=s.toLowerCase();
+System.out.println("Lower case of the string is:"+y);
+}
+}
