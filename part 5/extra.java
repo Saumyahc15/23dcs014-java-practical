1d2c5a800cee0994449602bef6d24370d6386856
Saumyahc15 <23dcs014@charusat.edu.in>
2024-09-09 11:26:16 +0530

    Create extra.java

---
+++ part 5/extra.java
@@ -0,0 +1,62 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class extra
+{
+	
+	public static void main(String []args)
+	{
+		/*int a=5,b=0,c;
+		System.out.println("before");
+		try{
+			
+			c=a/b;
+		}
+		catch(Exception e)
+		{                                                 
+			System.out.println("Exception solved " +e.toString());
+			
+		}
+          System.out.println("after");*/
+	
+		  
+		  
+		  
+		  /*int a[]={10,20,30};
+		  try{
+		  System.out.println(a[3]);
+		  }
+		  
+		  catch(Exception e)
+		  {
+			  System.out.println("Exception solved " + e.toString());
+			  
+		  }*/
+		  
+		  
+		  
+		  
+		  
+		 String s =new String("charusat");
+		 try{
+		  System.out.println(s.charAt(9));
+		 }
+		  catch(Exception k)
+		   {
+			  System.out.println("Exception solved " + k.toString());
+			  
+		  }
+		  
+		  
+		 /* String p=null;
+		  try{
+		  System.out.println(p.length());
+		  }
+		  catch(Exception h)
+		   {
+			  System.out.println("Exception solved " + h.toString());
+			  
+		  }*/
+	}
+	
+}
