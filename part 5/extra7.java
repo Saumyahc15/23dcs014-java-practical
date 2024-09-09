6e6500f4e6b7b25254f33a3a16b726e2940561e2
Saumyahc15 <23dcs014@charusat.edu.in>
2024-09-09 11:31:20 +0530

    Create extra7.java

---
+++ part 5/extra7.java
@@ -0,0 +1,15 @@
+class parent
+{
+void print()
+{
+System.out.println("This is parent class");
+}
+}
+
+class extra7 throws ClassNotFoundException
+{
+public static void main(String []args)
+{
+	child c=new child();
+}
+}
