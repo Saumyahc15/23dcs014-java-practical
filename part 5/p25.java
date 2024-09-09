62545a36f8f6089c1543b83cc3b80f01a9f86131
Saumyahc15 <23dcs014@charusat.edu.in>
2024-09-09 11:46:17 +0530

    Create p25.java

---
+++ part 5/p25.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+//Example of FileNotFoundException and
+//handling it using try and catch block
+class p25 {
+    public static void main(String[] args) {
+        try {
+            // Creating an instance of FileReader class
+            FileReader fileReader = new FileReader("input.txt");
+            System.out.println(fileReader.read());
+            fileReader.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
