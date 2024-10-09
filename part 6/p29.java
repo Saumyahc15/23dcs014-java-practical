f22da71284bcab60bb4600b91432ee64990f1ccf
Saumyahc15 <23dcs014@charusat.edu.in>
2024-10-09 20:48:52 +0530

    Create p29.java

---
+++ part 6/p29.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class p29 {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java WordSearch <file> <word>");
+            return;
+        }
+
+        String fileName = args[0];
+        String targetWord = args[1];
+
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            int lineCount = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains(targetWord)) {
+                    System.out.println("Word found on line " + (lineCount + 1) + ": " + line);
+                }
+                lineCount++;
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: File " + fileName + " not found.");
+        }
+    }
+}
